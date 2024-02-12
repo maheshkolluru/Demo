3ebc75edeb515b938391081844c44867ac3dc24e
user <user@DESKTOP-I1NF19H>
2024-02-12 15:49:18 +0530

    testname changed

---
+++ src/test/java/com/git/test/Tc02Test.java
@@ -0,0 +1,12 @@
+package com.git.test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc02Test {
+@Test
+public void tc2() {
+	Reporter.log("tc2 passed");
+	
+}
+}
