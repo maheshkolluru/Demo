edca90329aa8477592a79b55126ff133c1a672c1
user <user@DESKTOP-I1NF19H>
2024-02-13 15:15:14 +0530

    tc4 updated in eng2

---
+++ src/test/java/com/git/test/TC4.java
@@ -0,0 +1,14 @@
+package com.git.test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC4 {
+	@Test
+	public void tc4()
+	{
+		Reporter.log("tc4 updated in eng2");
+	}
+
+
+}
