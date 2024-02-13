fed1b91b47459c942fdec3c48cac5b85decdd989
user <user@DESKTOP-I1NF19H>
2024-02-13 15:12:26 +0530

    tc4 updated

---
+++ src/test/java/com/git/test/Tc4.java
@@ -0,0 +1,14 @@
+package com.git.test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc4 {
+	
+		@Test
+		public void git(){
+			
+			Reporter.log("tc4 passed");
+		}
+
+}
