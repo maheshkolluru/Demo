b1dbafa9d6e07ae98d1f24ca40c0e556989b1fda
user <user@DESKTOP-I1NF19H>
2024-02-13 15:34:54 +0530

    tc4 and tc5 updated

--- src/test/java/com/git/test/Tc5.java
+++ src/test/java/com/git/test/Tc5.java
@@ -3,12 +3,11 @@ package com.git.test;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class TC4 {
+public class Tc5 {
 	@Test
-	public void tc4()
-	{
-		Reporter.log("tc4 updated in eng2");
+	public void git(){
+		
+		Reporter.log("tc5 passed");
 	}
-
 
 }
